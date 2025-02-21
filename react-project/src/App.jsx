08c04719f223c3e6ff2644d12231281f7a45dc83
Tejash <tejashmaru2003@gmail.com>
2025-02-21 16:18:33 +0530

    Added components and local storage of todo

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NewTodoForm from './components/NewTodoForm'
+import TodoList from './components/TodoList'
 
 const App = () => {
+  
+  const [todos,setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS",JSON.stringify(todos))
+  },[todos])
+
+  function addTodo(title) {
+    setTodos((currentTodos)=> {return [...currentTodos,{ id: crypto.randomUUID(), title, completed: false},]})
+  }
+
+  
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id) {
+          return { ...todo, completed}
+        }
+        return todo
+      } )
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      console.log("Before:", currentTodos); // Old todos
+      const newTodos = currentTodos.filter(todo => todo.id !== id);
+      console.log("After:", newTodos)
+      console.log("After:",currentTodos)
+      return newTodos
+    })
+  }
   return (
-    <div>
-      
-    </div>
+    <>
+      <NewTodoForm onSubmit = {addTodo}/>
+      <h1 className='text-blue-600 text-3xl text-center mb-10 mt-10'>To Do List</h1>
+      <TodoList todos = {todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+
+    </>
   )
 }
 
